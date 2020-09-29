0a4d34da714fc6745071ae354f12d966f4fd4763
Mik <mihail.bindyukov@gmail.com>
2020-09-30 00:53:25 +0300

    feat: complete logic for all three functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 
 exports.min = function min (array) {
-  return 0;
+  // 0 - dummy, can be anything, as we don't need this
+  // array - is standard parameter to send parameters to the method
+  // standard math takes digits, from 0..n, so in apply array will be
+  // transformed in input parameters
+  return !array || array.length == 0 ? 0 : Math.min.apply(0, array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  // without appply we can just use ...array -> parse array for separate values for input 
+  return !array || array.length == 0 ? 0 :  Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return !array || array.length == 0 ? 0 :  array.reduce((value1, value2) => value1 + value2, 0) / array.length;
 }
